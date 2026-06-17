166e7ecbc9cda148863ab86995fc3e2e6d43be05
Yakov Didenko <kogotto@gmail.com>
2026-06-17 14:47:32 +0300

    Workers main function

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,1 +1,24 @@
 #include <ThreadPool.hpp>
+
+ThreadPool::ThreadPool(int workersCount) {
+    workers_.reserve(workersCount);
+    for (int i = 0; i < workersCount; ++i) {
+        workers_.emplace_back([this]{workerMain();});
+    }
+}
+
+void ThreadPool::workerMain() {
+    while (true) {
+        std::function<void()> task;
+        {
+            std::unique_lock lock{mutex};
+            cv.wait(lock, [this]{return done || !queue.empty();});
+            if (done && queue.empty()) {
+                return;
+            }
+            task = std::move(queue.front());
+            queue.pop();
+        }
+        task();
+    }
+}
--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+
+class ThreadPool {
+public:
+    ThreadPool(int workersCount);
+private:
+    void workerMain();
+    std::vector<std::jthread> workers_;
+    std::mutex mutex;
+    std::condition_variable cv;
+    std::queue<std::function<void()>> queue;
+    bool done{false};
+};
