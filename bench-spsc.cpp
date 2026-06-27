21cea9dcb509c0ac74935577099f96ac556d7527
Yakov Didenko <kogotto@gmail.com>
2026-06-27 12:00:42 +0300

    Add two simple benchmarks

--- bench-spsc.cpp
+++ bench-spsc.cpp
@@ -1,1 +1,24 @@
 #include <benchmark/benchmark.h>
+
+#include <string>
+
+namespace {
+
+void BM_longString(benchmark::State& state) {
+    for (auto&& _ : state) {
+        std::string s = "Some long string to avoid small string optimization";
+        benchmark::DoNotOptimize(s);
+    }
+}
+
+void BM_smallString(benchmark::State& state) {
+    for (auto&& _ : state) {
+        std::string s = "123";
+        benchmark::DoNotOptimize(s);
+    }
+}
+
+} // namespace
+
+BENCHMARK(BM_longString);
+BENCHMARK(BM_smallString);
