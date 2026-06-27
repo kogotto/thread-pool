4d0fbc336bb6e1b7e58bc347a98d47794808716a
Yakov Didenko <kogotto@gmail.com>
2026-06-27 10:31:00 +0300

    Add release build to gen.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 rm -rf build/
+rm -rf build-rel/
 rm compile_commands.json
--- gen.sh
+++ gen.sh
@@ -4,11 +4,13 @@ BUILD_DIR="build"
 OPTIONS="
 -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
--DCMAKE_BUILD_TYPE=Debug
 "
 
 mkdir $BUILD_DIR;
 
-cmake -S . -B $BUILD_DIR $OPTIONS;
+cmake -S . -B $BUILD_DIR $OPTIONS -DCMAKE_BUILD_TYPE=Debug;
 
 ln -sf $BUILD_DIR/compile_commands.json .;
+
+REL_BUILD_DIR="build-rel"
+cmake -S . -B $REL_BUILD_DIR $OPTIONS -DCMAKE_BUILD_TYPE=Release;
