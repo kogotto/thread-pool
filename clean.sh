2c5ac12b009f0d3a728fd24c8184313eff159425
kogotto <kogotto@gmail.com>
2026-09-16 09:41:44 +0300

    Add thread sanitizer build (#4)

--- clean.sh
+++ clean.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm -rf build/
-rm -rf build-rel/
-rm compile_commands.json
+source defs.sh
+
+rm -rf $BUILD_DIR
+rm -rf $REL_BUILD_DIR
+rm -rf $TSAN_BUILD_DIR
+rm -f compile_commands.json
