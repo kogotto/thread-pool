f124f8340d5fe25ef5acd7c8cd6fb3e4343591f0
Yakov Didenko <kogotto@gmail.com>
2026-09-21 13:48:42 +0300

    Add benchmark

---
+++ FetchAddCounterBench.cpp
@@ -0,0 +1,16 @@
+#include <benchmark/benchmark.h>
+
+#include <FetchAddCounter.hpp>
+
+namespace {
+
+void BM_FetchAddCounterBench(benchmark::State& state) {
+    for (auto&& _ : state) {
+        auto result = fetchAddCounterMain(1000);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+} // namespace
+
+BENCHMARK(BM_FetchAddCounterBench);
